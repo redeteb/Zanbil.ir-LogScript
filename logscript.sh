28742622e6b9b26b4e98e3a7c54e0d89ba9d9026
Redete Bezabeh <134902499+redeteb@users.noreply.github.com>
2024-08-20 15:03:50 -0500

    Delete logscript.sh

--- logscript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash/
-
-
-#Create a BASH script that will search the log file for 404 errors and identify the IP addresses associated with them
-
-
-grep " 404 " ./web-server-access-logs.log | awk '{print $1 "    "  $9}' > 404file.txt
